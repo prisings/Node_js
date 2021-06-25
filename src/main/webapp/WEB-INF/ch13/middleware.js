2434ca902f7657746dc10ecb3bebe1776a80a766
Jeong, HyeonGeon <hg1001@dawoncomputing.com>
2021-06-25 13:13:08 +0900

    node_js

---
+++ src/main/webapp/WEB-INF/ch13/middleware.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+app.use(function(req, res, next) {
+   var now = new Date();
+   console.log(now.toDateString() + ' - url : ' + req.url);
+   next();
+});
+
+app.use(function(req, res) {
+    res.send('Hello Express!! can not use again');
+
+});
+
+app.listen(3000);
